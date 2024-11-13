970303cabc947455db35c7406e6ad3bd6a2a47da
Diego Negrette <diegonegrette42@gmail.com>
2024-11-13 10:11:14 -0400

    Added last fixes

--- whatsapp-scraper/service/settings.py
+++ whatsapp-scraper/service/settings.py
@@ -176,8 +176,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = "static"
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
+STATIC_ROOT = f"{BASE_DIR}/staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
