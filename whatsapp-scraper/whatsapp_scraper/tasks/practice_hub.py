a38d85d75f820b26698a0d7aec2d127241b2a58e
Diego Negrette <diegonegrette42@gmail.com>
2024-11-27 15:09:26 -0400

    Fixed rounding when counting number of pages for patients info update

--- whatsapp-scraper/whatsapp_scraper/tasks/practice_hub.py
+++ whatsapp-scraper/whatsapp_scraper/tasks/practice_hub.py
@@ -1,4 +1,5 @@
 from dateutil.relativedelta import relativedelta
+import math
 import time
 
 from celery.utils.log import get_task_logger
@@ -105,7 +106,7 @@ def sync_missing_phone_numbers():
             logger.error(f"Something went wrong: {r}")
             return
         response = r.json()
-        total_pages = round(response["total_entries"] / 100)
+        total_pages = math.ceil(response["total_entries"] / 100)
         current_page += 1
         patients += response["data"]
         time.sleep(1)
@@ -113,16 +114,16 @@ def sync_missing_phone_numbers():
     patient_id_to_patient_info_dict = {patient["id"]: patient for patient in patients}
     appointments_to_update = []
     for appointment in target_appointments:
-        pattient_info = patient_id_to_patient_info_dict.get(appointment.patient_id)
+        patient_info = patient_id_to_patient_info_dict.get(appointment.patient_id)
         selected_phone_number = None
-        if pattient_info and len(pattient_info["numbers"]) >= 1:
-            for phone_info in pattient_info["numbers"]:
+        if patient_info and len(patient_info["numbers"]) >= 1:
+            for phone_info in patient_info["numbers"]:
                 if phone_info["country_code"] and phone_info["number"]:
                     selected_phone_number = f"+{phone_info['country_code']}{phone_info['number'].replace('(','').replace(')','').replace(' ', '').replace('-', '')}"
                     break
         if selected_phone_number:
             appointment.patient_phone_number = selected_phone_number
-            appointment.language = pattient_info["locale"]
+            appointment.language = patient_info["locale"]
             appointments_to_update.append(appointment)
             logger.info(
                 f"Updated missing phone number for appointment {appointment.id}"
